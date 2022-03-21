21fa9ae1bc94c9615ed83294bcb8f7c381d557af
samuele spadaro <spadaro.samuele@gmail.com>
2022-03-21 17:59:52 +0100

    bonus fatto

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,35 +2,70 @@
 //Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 let box = document.getElementById("box")
 
+let livelli = document.getElementById("livelli");
+
+let bottone = document.getElementById("bottone");
+
+var celleLivello = 0;
+
 //funzione per i numeri random
 // function numeriRandom ( num1, num2 ){
 //     return Math.floor(Math.random()* num1) + num2;
 // }
 
+bottone.addEventListener("click",
+    function () {
+        console.log(livelli.value)
 
-for ( i = 1 ; i <= 100; i ++ ){
-    let divContainer = document.createElement("div");
-    //aggiungere una calsse al div creatp
-    divContainer.classList.add("cell")
-    //aggiungere i numeri alla singola cella
-    divContainer.innerHTML = i
+        if (livelli.value == "livello medio") {
+            celleLivello = 81;
+            document.documentElement.style.setProperty("--numero-celle", "9");
 
+        } else if (livelli.value == "livello difficile") {
+            celleLivello = 49;
+            document.documentElement.style.setProperty("--numero-celle", "7");
 
-    //appendere l'elemento creato dentro il div in html con id box
-    box.appendChild(divContainer);
-     
-    //fumzione evento cliccato
-    divContainer.addEventListener( "click",
-        function(){
-            console.log(this);
+        } else {
+            celleLivello = 100;
 
-            //mettere una classe al this
-            this.classList.add("color-cell")
         }
+
+        for (i = 1; i <= celleLivello; i++) {
+            let divContainer = document.createElement("div");
+            //aggiungere una calsse al div creatp
+            divContainer.classList.add("cell-100")
+            //aggiungere i numeri alla singola cella
+            divContainer.innerHTML = i
         
-    )
+        
+            //appendere l'elemento creato dentro il div in html con id box
+            box.appendChild(divContainer);
+        
+            //fumzione evento cliccato
+            divContainer.addEventListener("click",
+                function () {
+                    console.log(this);
+        
+                    //mettere una classe al this
+                    this.classList.add("color-cell")
+                }
+        
+            )
+        
+        }
+    }
 
-}    
+)
+
+
+
 console.log(box)
-    
 
+
+
+
+//bonus
+// Permettere all'utente di indicare una difficoltà in base alla quale viene generato un numero variabile di celle:
+// con difficoltà 1 => tra 1 e 100
+// con difficoltà 2 => tra 1 e 81
+// con difficoltà 3 => tra 1 e 49
